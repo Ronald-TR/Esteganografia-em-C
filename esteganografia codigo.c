fb23c8452ce568c60421a6b1315a58d6ced21fcc
Ronald <ronald-farias@outlook.com>
2017-01-24 23:29:15 -0300

    Update esteganografia codigo.c

--- esteganografia codigo.c
+++ esteganografia codigo.c
@@ -3,11 +3,11 @@
 #define MAX 999
 int main (){
 
-int tam_extensions=10, i=0, k=0, tam_extensions_txt=2;//variaveis de tamanho da string extensıes e contador
-char string[MAX];//string que armazenar· os paths do usuario
-char extensions[10][10]={".jpg", ".jpg\"", ".jpeg", ".jpeg\"", ".png", ".png\"", ".gif", ".gif\"", ".bmp", ".bmp\""};//string de extensıes pra comparaÁ„o
+int tam_extensions=10, i=0, k=0, tam_extensions_txt=2;//variaveis de tamanho da string extens√µes e contador
+char string[MAX];//string que armazenar√° os paths do usuario
+char extensions[10][10]={".jpg", ".jpg\"", ".jpeg", ".jpeg\"", ".png", ".png\"", ".gif", ".gif\"", ".bmp", ".bmp\""};//string de extens√µes pra compara√ß√£o
 char extensions_txt[2][5]={".txt", ".txt\""};
-char comand[MAX]="copy /b ";//string para concatenar o comando para a funÁ„o system executar
+char comand[MAX]="copy /b ";//string para concatenar o comando para a fun√ß√£o system executar
 printf("Wellcome to a simple esteganografia-script based on C and system commands\nMade by Ronald\nEnjoy!\n");
 printf("Use this script if you want to input a text file inside a img code. (you can drag and drop here).\n");
 printf ("Insert path from img file\n");
@@ -18,17 +18,17 @@ while (1)//compara se o comando dado possui uma imagem
 
     for(i=0; i<tam_extensions; i++){
         if(strstr(string, extensions[i])==NULL){
-            k++;//se n„o achar uma ocorrencia da extens„o, incrementa k
+            k++;//se n√£o achar uma ocorrencia da extens√£o, incrementa k
         }
     }
-if(k==i){//se k for igual a i, significa que todas as posiÁıes do loop eram nulas a comparaÁ„o, ent„o alerta pra inserir um arquivo valido
+if(k==i){//se k for igual a i, significa que todas as posi√ß√µes do loop eram nulas a compara√ß√£o, ent√£o alerta pra inserir um arquivo valido
     printf("Please insert a file with a valid img extension path\n");
     setbuf(stdin, NULL);
     fgets(string, MAX, stdin);
     string[strlen(string)-1]='\0';
     k=0;
 
-    }else {//reinsere as aspas retiradas e sai da repetiÁ„o
+    }else {//reinsere as aspas retiradas e sai da repeti√ß√£o
         k=0;
         break;}
 }
@@ -37,23 +37,23 @@ strcat(comand,"+");
 printf ("Insert path from txt file\n");
 fgets(string, MAX, stdin);
 string[strlen(string)-1]='\0';
-while(1)//compara se o caminho dado È de um arquivo de extens„o txt
+while(1)//compara se o caminho dado √© de um arquivo de extens√£o txt
 {
 
 
     for(i=0; i<tam_extensions_txt; i++){
         if(strstr(string, extensions_txt[i])==NULL){
-            k++;//se n„o achar uma ocorrencia da extens„o, incrementa k
+            k++;//se n√£o achar uma ocorrencia da extens√£o, incrementa k
         }
     }
-if(k==i){//se k for igual a i, significa que todas as posiÁıes do loop eram nulas a comparaÁ„o, ent„o alerta pra inserir um arquivo valido
+if(k==i){//se k for igual a i, significa que todas as posi√ß√µes do loop eram nulas a compara√ß√£o, ent√£o alerta pra inserir um arquivo valido
     printf("Please insert a file with a valid img extension path\n");
     setbuf(stdin, NULL);
     fgets(string, MAX, stdin);
     string[strlen(string)-1]='\0';
     k=0;
 
-    }else {//reinsere as aspas retiradas e sai da repetiÁ„o
+    }else {//sai da repeti√ß√£o
         break;}
 }
 
